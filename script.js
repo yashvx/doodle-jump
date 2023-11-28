0a2603c9761f8e1a9aaa49b0efd2e459ac104cd7
yashvx <144120010+yashvx@users.noreply.github.com>
2023-11-28 15:29:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,253 @@
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 576;
+let context;
+
+//doodler
+let doodlerWidth = 46;
+let doodlerHeight = 46;
+let doodlerX = boardWidth / 2 - doodlerWidth / 2;
+let doodlerY = boardHeight - doodlerHeight;
+let doodlerRightImg;
+let doodlerLeftImg;
+
+let doodler = {
+  img: null,
+  x: doodlerX,
+  y: doodlerY,
+  width: doodlerWidth,
+  height: doodlerHeight,
+};
+
+//physics
+let velocityX = 0;
+let velocityY = 0;
+let initialVelocityY = -8;
+let gravity = 0.4;
+
+function displayUsername() {
+  var username = document.getElementById("usernameInput").value;
+
+  var displayElement = document.getElementById("usernameDisplay");
+  displayElement.textContent = "Welcome, " + username + "!";
+  displayElement.style.position = "fixed";
+  displayElement.style.top = "10px";
+  displayElement.style.right = "10px";
+  displayElement.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+  displayElement.style.padding = "10px";
+  displayElement.style.borderRadius = "5px";
+}
+
+//platforms
+let platformArray = [];
+let platformWidth = 60;
+let platformHeight = 18;
+let platformImg;
+
+let score = 0;
+let maxScore = 0;
+let gameOver = false;
+
+const body = document.getElementsByTagName("body")[0];
+window.onload = function () {
+  const playButton = document.getElementById("play-button");
+  playButton.addEventListener("click", renderGame);
+};
+
+function renderGame() {
+  body.style.backgroundImage = "url()";
+
+  board = document.createElement("canvas");
+  board.setAttribute("id", "board");
+  board.style.backgroundImage = "url(./doodlejumpbg.png)";
+
+  board.height = boardHeight;
+  board.width = boardWidth;
+
+  body.replaceChildren(board);
+  context = board.getContext("2d");
+
+  //load images
+  doodlerRightImg = new Image();
+  doodlerRightImg.src = "./doodler-right.png";
+  doodler.img = doodlerRightImg;
+  doodlerRightImg.onload = function () {
+    context.drawImage(
+      doodler.img,
+      doodler.x,
+      doodler.y,
+      doodler.width,
+      doodler.height
+    );
+  };
+
+  doodlerLeftImg = new Image();
+  doodlerLeftImg.src = "./doodler-left.png";
+
+  platformImg = new Image();
+  platformImg.src = "./platform.png";
+
+  velocityY = initialVelocityY;
+  placePlatforms();
+  requestAnimationFrame(update);
+  document.addEventListener("keydown", moveDoodler);
+}
+
+function update() {
+  requestAnimationFrame(update);
+  if (gameOver) {
+    return;
+  }
+  context.clearRect(0, 0, board.width, board.height);
+
+  //doodler
+  doodler.x += velocityX;
+  if (doodler.x > boardWidth) {
+    doodler.x = 0;
+  } else if (doodler.x + doodler.width < 0) {
+    doodler.x = boardWidth;
+  }
+
+  velocityY += gravity;
+  doodler.y += velocityY;
+  if (doodler.y > board.height) {
+    gameOver = true;
+  }
+  context.drawImage(
+    doodler.img,
+    doodler.x,
+    doodler.y,
+    doodler.width,
+    doodler.height
+  );
+
+  //platforms
+  for (let i = 0; i < platformArray.length; i++) {
+    let platform = platformArray[i];
+    if (velocityY < 0 && doodler.y < (boardHeight * 3) / 4) {
+      platform.y -= initialVelocityY; //slide platform down
+    }
+    if (detectCollision(doodler, platform) && velocityY >= 0) {
+      velocityY = initialVelocityY; //jump
+    }
+    context.drawImage(
+      platform.img,
+      platform.x,
+      platform.y,
+      platform.width,
+      platform.height
+    );
+  }
+
+  //clearing platforms and adding thw new platforms
+  while (platformArray.length > 0 && platformArray[0].y >= boardHeight) {
+    platformArray.shift(); //removes first element from the array
+    newPlatform(); //replace with new platform on top
+  }
+
+  //score
+  updateScore();
+  context.fillStyle = "black";
+  context.font = "16px sans-serif";
+  context.fillText(score, 5, 20);
+
+  if (gameOver) {
+    context.fillText(
+      "Game Over: Press 'Space' to Restart",
+      boardWidth / 7,
+      (boardHeight * 7) / 8
+    );
+  }
+}
+
+function moveDoodler(e) {
+  if (e.code == "ArrowRight" || e.code == "KeyD") {
+    //move right
+    velocityX = 4;
+    doodler.img = doodlerRightImg;
+  } else if (e.code == "ArrowLeft" || e.code == "KeyA") {
+    //move left
+    velocityX = -4;
+    doodler.img = doodlerLeftImg;
+  } else if (e.code == "Space" && gameOver) {
+    //reset
+    doodler = {
+      img: doodlerRightImg,
+      x: doodlerX,
+      y: doodlerY,
+      width: doodlerWidth,
+      height: doodlerHeight,
+    };
+
+    velocityX = 0;
+    velocityY = initialVelocityY;
+    score = 0;
+    maxScore = 0;
+    gameOver = false;
+    placePlatforms();
+  }
+}
+
+function placePlatforms() {
+  platformArray = [];
+
+  //starting platforms
+  let platform = {
+    img: platformImg,
+    x: boardWidth / 2,
+    y: boardHeight - 50,
+    width: platformWidth,
+    height: platformHeight,
+  };
+
+  platformArray.push(platform);
+
+  for (let i = 0; i < 6; i++) {
+    let randomX = Math.floor((Math.random() * boardWidth * 3) / 4);
+    let platform = {
+      img: platformImg,
+      x: randomX,
+      y: boardHeight - 75 * i - 150,
+      width: platformWidth,
+      height: platformHeight,
+    };
+
+    platformArray.push(platform);
+  }
+}
+
+function newPlatform() {
+  let randomX = Math.floor((Math.random() * boardWidth * 3) / 4);
+  let platform = {
+    img: platformImg,
+    x: randomX,
+    y: -platformHeight,
+    width: platformWidth,
+    height: platformHeight,
+  };
+
+  platformArray.push(platform);
+}
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width && //a's top left corner doesn't reach b's top right corner
+    a.x + a.width > b.x && //a's top right corner passes b's top left corner
+    a.y < b.y + b.height && //a's top left corner doesn't reach b's bottom left corner
+    a.y + a.height > b.y
+  ); //a's bottom left corner passes b's top left corner
+}
+
+function updateScore() {
+  let points = Math.floor(50 * Math.random());
+  if (velocityY < 0) {
+    //negative going up
+    maxScore += points;
+    if (score < maxScore) {
+      score = maxScore;
+    }
+  } else if (velocityY >= 0) {
+    maxScore -= points;
+  }
+}
